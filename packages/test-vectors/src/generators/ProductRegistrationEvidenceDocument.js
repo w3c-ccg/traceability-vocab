57535ef93f046dddcc07a78eb462cad95b137ad8
andrey.orlov.od@gmail.com <andrey.orlov.od@gmail.com>
2020-12-29 09:17:57 +0200

    feature/Ecommerce-Product-VC update fake data

--- packages/test-vectors/src/generators/ProductRegistrationEvidenceDocument.js
+++ packages/test-vectors/src/generators/ProductRegistrationEvidenceDocument.js
@@ -2,6 +2,21 @@ const faker = require('faker');
 
 const getProductRegistrationEvidenceDocument = () => {
   const pastDate = new Date(faker.date.past());
+  const getHTScode = () => {
+    const n1 = faker.random.number({min: 1000, max: 9999})
+    const n2 = faker.random.number({min: 10, max: 99})
+    const n3 = faker.random.number({min: 1000, max: 9999})
+
+    return `${n1}.${n2}.${n3}`;
+  };
+
+  const getNormalizeMetricNumber = () => {
+    const randNumber1 = faker.random.number({min: 1000, max: 9999});
+
+
+    return randNumber2;
+  };
+
   const example = {
     '@context': ['https://w3id.org/traceability/v1'],
     type: 'ProductRegistrationEvidenceDocument',
@@ -9,25 +24,25 @@ const getProductRegistrationEvidenceDocument = () => {
     "inProductGroupWithID": faker.lorem.sentence(),
     "productID": faker.random.uuid(),
     "mpn": faker.lorem.word(),
-    "gtin": faker.internet.url(),
+    "gtin": `${faker.random.number({min: 100000000000, max: 99999999999999})}`,
     "isAccessoryOrSparePartFor": faker.lorem.sentence(),
     "releaseDate": pastDate.getMonth() + "-" + pastDate.getDay() + "-" + pastDate.getFullYear(),
     "manufacturer": faker.lorem.word(),
-    "globalLocationNumber": faker.address.secondaryAddress(),
+    "globalLocationNumber": `${faker.random.number({min: 1000000000000, max: 9999999999999})}`,
     "leiCode": faker.address.secondaryAddress(),
     "name": faker.lorem.word(),
     "description": faker.lorem.paragraph(),
     "model": faker.random.uuid(),
     "color": faker.internet.color(),
     "material": faker.lorem.word(),
-    "weight": `${faker.random.number()} kg`,
-    "height": `${faker.random.number()} m`,
-    "width": `${faker.random.number()} m`,
-    "depth": `${faker.random.number()} m`,
+    "weight": `${faker.random.number({min: 1, max: 10, precision: 0.001})} kg`,
+    "height": `${faker.random.number({min: 1, max: 10, precision: 0.001})} m`,
+    "width": `${faker.random.number({min: 1, max: 10, precision: 0.001})} m`,
+    "depth": `${faker.random.number({min: 1, max: 10, precision: 0.001})} m`,
     "url": faker.internet.url(),
     "isBasedOn": faker.internet.url(),
     "image": faker.image.imageUrl(),
-    "HTScode": faker.random.uuid()
+    "HTScode": getHTScode()
   };
 
   return example;
