b8ba9513d10c18fb969851af95f87aed90933976
mprorock <mprorock@mesur.io>
2021-02-27 12:03:35 -0500

    commit after full lint, build, test cycle post upstream merge

--- packages/traceability-vocab/src/generators/EcommerceWayBillCommodityGroup.js
+++ packages/traceability-vocab/src/generators/EcommerceWayBillCommodityGroup.js
@@ -1,28 +1,25 @@
-
 const faker = require('faker');
 const { getEcommerceWayBillTotals } = require('./EcommerceWayBillTotals');
 
-
 const getEcommerceWayBillCommodityGroup = () => {
-
 // get a currency
-    
-let commodityItemNumber = faker.random.alphaNumeric(12);
 
-const getnatureQuantityOfGoods = () => {
-    const types = ['A lot of laptops that way a lot','A lot fo stuffed animals, and toys and other useless stuff that weighs a lot'];
+  const commodityItemNumber = faker.random.alphaNumeric(12);
+
+  const getnatureQuantityOfGoods = () => {
+    const types = ['A lot of laptops that way a lot', 'A lot fo stuffed animals, and toys and other useless stuff that weighs a lot'];
     return faker.random.arrayElement(types);
   };
 
-let totalsCommodityGroup = getEcommerceWayBillTotals();
-delete totalsCommodityGroup['@context'];
-    
+  const totalsCommodityGroup = getEcommerceWayBillTotals();
+  delete totalsCommodityGroup['@context'];
+
   const example = {
     '@context': ['https://w3id.org/traceability/v1'],
     type: 'EcommerceWayBillCommodityGroup',
     commodityItemNumber,
     natureQuantityOfGoods: getnatureQuantityOfGoods(),
-    totalsCommodityGroup
+    totalsCommodityGroup,
   };
   return example;
 };
