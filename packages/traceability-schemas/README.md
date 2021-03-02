5287555f599e8d282cdd90dfda98e5b159efd942
Orie Steele <orie@transmute.industries>
2021-03-02 17:11:22 -0600

    chore: wip refactor build

---
+++ packages/traceability-schemas/README.md
@@ -0,0 +1,3 @@
+# Traceability Schemas
+
+This module contains all the JSON Schema used to build the traceability vocab.
---
+++ packages/traceability-schemas/scripts/build-intermediate.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+
+console.log("🧪 build intermediate json from schemas");
+
+const getIntermediateFromDirectory = require("./getIntermediateFromDirectory");
+
+const intermediateJson = getIntermediateFromDirectory(
+  path.resolve(__dirname, "../schemas")
+);
+
+console.log(intermediateJson);
+
+fs.writeFileSync(
+  path.resolve(__dirname, "../../../docs/intermediate.json"),
+  JSON.stringify(intermediateJson, null, 2)
+);
