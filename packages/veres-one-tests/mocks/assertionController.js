78a3db0c7b63416b909f57a63775631b529c13f6
mprorock <mprorock@mesur.io>
2021-03-06 21:51:16 -0500

    fix: removed a lost package

--- packages/veres-one-tests/mocks/assertionController.js
+++
@@ -1,8 +0,0 @@
-const assertionController = {
-  '@context': 'https://w3id.org/security/v2',
-  id: 'https://example.edu/issuers/565049',
-  // actual keys are going to be added in the test suite before() block
-  assertionMethod: [],
-  authentication: []
-};
-module.exports = assertionController;
