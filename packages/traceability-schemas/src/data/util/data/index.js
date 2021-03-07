1b30cfa1cae903671aa95e301ce31a997e02da65
mprorock <mprorock@mesur.io>
2021-03-07 12:08:13 -0500

    chore: LEIentity dates

--- packages/traceability-schemas/src/data/util/data/index.js
+++ packages/traceability-schemas/src/data/util/data/index.js
@@ -6,6 +6,7 @@ class DataGenerator {
         this.dates.prior = '2020-01-07';
         this.dates.current = '2021-03-09';
         this.dates.future = '2022-05-11';
+        this.dates.futureAlt = '2022-07-11';
     }
 }
 const generator = new DataGenerator();
