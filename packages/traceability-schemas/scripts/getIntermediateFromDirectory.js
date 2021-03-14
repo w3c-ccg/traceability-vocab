1cef8985d00652a8f2d6cdd32f139d5ed0fbcda4
Orie Steele <orie@transmute.industries>
2021-03-14 18:12:09 -0500

    chore: fix dupe issue

--- packages/traceability-schemas/scripts/getIntermediateFromDirectory.js
+++ packages/traceability-schemas/scripts/getIntermediateFromDirectory.js
@@ -1,49 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
+const jsonldSchema = require('@transmute/jsonld-schema');
+
 const getAllJsonFilesFromDirectory = (targetDirectory) => {
   const files = fs
     .readdirSync(targetDirectory)
     .filter((f) => f.indexOf('.json') !== -1);
-  return files.map((f) => JSON.parse(fs.readFileSync(path.resolve(targetDirectory, f).toString())));
+  return files.map((f) => {
+    const c = fs.readFileSync(path.resolve(targetDirectory, f).toString());
+    return JSON.parse(c);
+  });
 };
 
 const getIntermediateFromDirectory = (targetDirectory) => {
-  const intermediate = {};
   const files = getAllJsonFilesFromDirectory(targetDirectory);
-  files.forEach((file) => {
-    if (file.$comment) {
-      const $classComment = JSON.parse(file.$comment);
-      if (!intermediate[$classComment['@id']]) {
-        intermediate[$classComment['@id']] = {
-          $id: file.$id,
-          $schema: file.$schema,
-          $comment: $classComment,
-          title: file.title,
-          description: file.description,
-          classProperties: {},
-          examples: file.examples,
-          schema: file,
-        };
-      }
-
-      if (file.properties) {
-        Object.values(file.properties).forEach((prop) => {
-          if (prop.$comment) {
-            const $propertyComment = JSON.parse(prop.$comment);
-            intermediate[$classComment['@id']].classProperties[
-              $propertyComment['@id']
-            ] = {
-              $comment: $propertyComment,
-              title: prop.title,
-              description: prop.description,
-            };
-          }
-        });
-      }
-    }
-  });
-  return intermediate;
+  return jsonldSchema.schemasToIntermediate(files);
 };
 
 module.exports = getIntermediateFromDirectory;
