c587e30d6c9a4c66315122376c49073893bdac1b
Mahmoud Alkhraishi <mkhraisha@hotmail.com>
2021-05-25 09:06:58 -0400

    delete child contexts

--- packages/traceability-schemas/src/generators/Event.js
+++ packages/traceability-schemas/src/generators/Event.js
@@ -7,9 +7,15 @@ const getEvent = () => {
     const eventType = 'commission';
     const eventId = '12345';
     const actor = [getOrganization(), getOrganization()];
+
+    delete actor[0]['@context'];
+    delete actor[1]['@context'];
     const eventLocation = getPlace();
+    delete eventLocation['@context'];
     const eventTime = '';
     const products = [getProduct(), getProduct()];
+    delete products[0]['@context'];
+    delete products[1]['@context'];
 
     const example = {
         '@context': ['https://w3id.org/traceability/v1'],
