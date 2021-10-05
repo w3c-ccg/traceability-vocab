c751271673e944a500f67e1c22ff68dc94abc794
Nis Jespersen <34443212+nissimsan@users.noreply.github.com>
2021-10-05 19:11:04 +0200

    Update TransportEquipment.js

--- packages/traceability-schemas/src/generators/TransportEquipment.js
+++ packages/traceability-schemas/src/generators/TransportEquipment.js
@@ -9,32 +9,3 @@ const getTransportEquipment = () => {
 
 module.exports = { getTransportEquipment };
 
-  //   "utilizedTransportEquipments": [
-  //     {
-  //       "equipment": {
-  //         "equipmentReference": "APZU4812090",
-  //         "ISOEquipmentCode": "stri",
-  //         "tareWeight": 12000,
-  //         "weightUnit": "KGM",
-  //         "isShipperOwned": true
-  //       },
-  //       "cargoGrossWeightUnit": "KGM",
-  //       "cargoGrossWeight": 12000,
-  //       "activeReeferSettings": {
-  //         "temperatureMin": -18.1,
-  //         "temperatureMax": 64.3,
-  //         "temperatureUnit": "CEL",
-  //         "humidityMin": 80,
-  //         "humidityMax": 100,
-  //         "ventilationMin": 266,
-  //         "ventilationMax": 296
-  //       },
-  //       "seals": [
-  //         {
-  //           "sealNumber": "string",
-  //           "sealSource": "CUS",
-  //           "sealType": "WIR"
-  //         }
-  //       ]
-  //     }
-  //   ],
