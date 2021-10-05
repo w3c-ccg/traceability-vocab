4e32714a2396717b7bb5ec66f154ff7e9d2eb46e
Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
2021-10-05 22:26:28 +0000

    ci: version bump to v0.0.4

--- packages/traceability-schemas/src/generators/TransportDocument.js
+++ packages/traceability-schemas/src/generators/TransportDocument.js
@@ -11,4 +11,3 @@ const getTransportDocument = () => {
 };
 
 module.exports = { getTransportDocument };
-
--- packages/traceability-schemas/src/generators/TransportEquipment.js
+++ packages/traceability-schemas/src/generators/TransportEquipment.js
@@ -8,4 +8,3 @@ const getTransportEquipment = () => {
 };
 
 module.exports = { getTransportEquipment };
-
