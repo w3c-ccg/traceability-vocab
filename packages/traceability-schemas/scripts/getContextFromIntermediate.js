936543c9ccd996da4209d2fc9e495bccc37f6f81
Russell Hofvendahl <russell.hofvendahl@mesur.io>
2021-11-05 12:59:08 -0700

    chore: make use of updated intermediateToPartialContext

--- packages/traceability-schemas/scripts/getContextFromIntermediate.js
+++ packages/traceability-schemas/scripts/getContextFromIntermediate.js
@@ -2,8 +2,17 @@ const jsonldSchema = require('@transmute/jsonld-schema');
 const rootTerms = require('../rootTerms.json');
 
 const getContextFromIntermediate = (intermediate) => {
-  const version = 1.1;
-  return jsonldSchema.intermediateToContext(intermediate, version, rootTerms);
+  const partialContext = jsonldSchema.intermediateToPartialContext(intermediate);
+  return {
+    '@context': {
+      '@version': 1.1,
+      '@vocab': 'https://w3id.org/traceability/#undefinedTerm',
+      id: '@id',
+      type: '@type',
+      ...rootTerms,
+      ...partialContext,
+    },
+  };
 };
 
 module.exports = getContextFromIntermediate;
