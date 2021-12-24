7fd89a56b12a724b4e5b9ff39b8f1c2ff508bbf5
Orie Steele <orie@transmute.industries>
2021-12-24 10:35:52 -0600

    chore: tests

--- packages/traceability-schemas/__tests__/schemas-sanity.test.js
+++ packages/traceability-schemas/__tests__/schemas-sanity.test.js
@@ -7,45 +7,46 @@ it('schemas is an array', () => {
   expect(Array.isArray(schemas)).toBe(true);
 });
 
-// it('all schemas have an example', () => {
-//   expect(
-//     schemas.every((s) => {
-//       const p = typeof s.example === 'string';
-//       if (!p) {
-//         console.warn('missing example: ', s);
-//       }
-//       return p;
-//     })
-//   ).toBe(true);
-// });
+it('all schemas have an example', () => {
+  expect(
+    schemas.every((s) => {
+      const p = typeof s.example === 'string';
+      if (!p) {
+        console.warn('missing example: ', s);
+      }
+      return p;
+    })
+  ).toBe(true);
+});
 
-// it('all schemas examples are valid json', () => {
-//   expect(
-//     schemas.every((s) => {
-//       try {
-//         JSON.parse(s.example);
-//       } catch (e) {
-//         console.warn('failed to parse json: ', s);
-//         return false;
-//       }
-//       return true;
-//     })
-//   ).toBe(true);
-// });
+it('all schemas examples are valid json', () => {
+  expect(
+    schemas.every((s) => {
+      try {
+        JSON.parse(s.example);
+      } catch (e) {
+        console.warn('failed to parse json: ', s);
+        return false;
+      }
+      return true;
+    })
+  ).toBe(true);
+});
 
-// it('all schemas examples are valid json-ld', async () => {
-//   const checks = await Promise.all(
-//     schemas.map(async (s) => {
-//       const input = JSON.parse(s.example);
-//       const { ok, error } = await check({
-//         input: { '@context': 'https://w3id.org/traceability/v1', ...input },
-//         documentLoader,
-//       });
-//       if (!ok) {
-//         console.warn('JSON-LD Error: ', error);
-//       }
-//       return ok;
-//     })
-//   );
-//   expect(checks.every((s) => s)).toBe(true);
-// });
+// due to the use of @vocab, we expect this to almost never error.
+it('all schemas examples are valid json-ld', async () => {
+  const checks = await Promise.all(
+    schemas.map(async (s) => {
+      const input = JSON.parse(s.example);
+      const { ok, error } = await check({
+        input: { '@context': 'https://w3id.org/traceability/v1', ...input },
+        documentLoader,
+      });
+      if (!ok) {
+        console.warn('JSON-LD Error: ', error);
+      }
+      return ok;
+    })
+  );
+  expect(checks.every((s) => s)).toBe(true);
+});
--- packages/traceability-schemas/services/documentLoader.js
+++ packages/traceability-schemas/services/documentLoader.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const path = require('path');
+const cred = require('@transmute/credentials-context');
 
 const contexts = {
+  'https://www.w3.org/2018/credentials/v1': cred.contexts.get(
+    'https://www.w3.org/2018/credentials/v1'
+  ),
   'https://w3id.org/traceability/v1': JSON.parse(
     fs
       .readFileSync(
