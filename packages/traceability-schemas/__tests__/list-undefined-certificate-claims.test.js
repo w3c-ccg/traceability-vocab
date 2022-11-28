b96123bdad769356f94415b408f8b55446b5da5f
Benjamin Collins <benjamin@collins.moe>
2022-11-29 03:58:43 +0900

    debug: update timeout value

--- packages/traceability-schemas/__tests__/list-undefined-certificate-claims.test.js
+++ packages/traceability-schemas/__tests__/list-undefined-certificate-claims.test.js
@@ -8,7 +8,7 @@ const documentLoader = require('../services/documentLoader');
 
 const { schemas } = require('../services/schemas');
 
-jest.setTimeout(30000);
+jest.setTimeout(10000);
 
 const verifiableCredentials = schemas
   .map((s) => {
