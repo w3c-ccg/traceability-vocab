27f873a27cb384976535897e3bc0c59bc494bd48
Orie Steele <orie@transmute.industries>
2023-04-12 14:00:31 -0500

    Update packages/traceability-tests/tests/jsonld.test.js
    Co-authored-by: Mike Prorock <mprorock@users.noreply.github.com>

--- packages/traceability-tests/tests/jsonld.test.js
+++ packages/traceability-tests/tests/jsonld.test.js
@@ -13,7 +13,7 @@ it('canonize', async () => {
       {
         '@context': { '@vocab': 'https://www.w3.org/2018/credentials#' },
         type: ['VerifiableCredential'],
-        issuer: 'https://example.edu/issuers/565049',
+        issuer: 'https://example.org/issuers/565049',
         issuanceDate: '2010-01-01T19:23:24Z',
         credentialSubject: {
           id: 'did:example:ebfeb1f712ebc6f1c276e12ec21',
