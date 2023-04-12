b2b857b3b37c9adf67a6b14d2b8d23728a6bef0c
Orie Steele <orie@transmute.industries>
2023-04-12 14:00:38 -0500

    Update packages/traceability-tests/tests/jsonld.test.js
    Co-authored-by: Mike Prorock <mprorock@users.noreply.github.com>

--- packages/traceability-tests/tests/jsonld.test.js
+++ packages/traceability-tests/tests/jsonld.test.js
@@ -27,7 +27,7 @@ it('canonize', async () => {
     .toBe(`_:c14n0 <https://www.w3.org/2018/credentials#id> "did:example:ebfeb1f712ebc6f1c276e12ec21" .
 _:c14n1 <https://www.w3.org/2018/credentials#credentialSubject> _:c14n0 .
 _:c14n1 <https://www.w3.org/2018/credentials#issuanceDate> "2010-01-01T19:23:24Z" .
-_:c14n1 <https://www.w3.org/2018/credentials#issuer> "https://example.edu/issuers/565049" .
+_:c14n1 <https://www.w3.org/2018/credentials#issuer> "https://example.org/issuers/565049" .
 _:c14n1 <https://www.w3.org/2018/credentials#type> "VerifiableCredential" .
 `);
 });
