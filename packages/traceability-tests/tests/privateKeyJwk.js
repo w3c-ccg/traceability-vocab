2d6ab311ea3aebe4137129ab55882d12043c5a7d
Orie Steele <orie@transmute.industries>
2023-05-21 13:16:51 -0500

    Updates

--- packages/traceability-tests/tests/privateKeyJwk.js
+++
@@ -1,8 +0,0 @@
-const privateKeyJwk = {
-  kty: 'OKP',
-  crv: 'Ed25519',
-  x: 'zX2zoGmW36TTL_kw3g-KFVjh5IoaDcoSc08jID4uGrs',
-  d: '95hNcacuqtWQP70MDYrL6DRUJ8iGg-oq13I0KM5Etw8',
-};
-
-module.exports = privateKeyJwk;
