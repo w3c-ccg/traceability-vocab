56cabab4e113370ff8883a054aa3b7a93c0033e1
Mahmoud Alkhraishi <mkhraisha@hotmail.com>
2024-10-10 15:29:48 -0400

    updates to context v2

--- packages/traceability-tests/tests/sanity/context-validation.test.js
+++ packages/traceability-tests/tests/sanity/context-validation.test.js
@@ -1,12 +1,12 @@
-const Ajv = require('ajv');
-const yaml = require('js-yaml');
+const Ajv = require('ajv')
+const yaml = require('js-yaml')
 
 const ajv = new Ajv({
   strict: false,
   // In order to get strict mode back on, and warnings off,
   // We may need these if statements in the future
   // https://github.com/ajv-validator/ajv/issues/1417#issuecomment-1431435887
-});
+})
 
 describe('v2', () => {
   const validate = ajv.compile(
@@ -19,11 +19,11 @@ properties:
     type: array
     readOnly: true
     default:
-      - https://www.w3.org/2018/credentials/v2
+      - https://www.w3.org/ns/credentials/v2
       - https://w3id.org/traceability/v1
     items:
       - type: string
-        const: https://www.w3.org/2018/credentials/v2
+        const: https://www.w3.org/ns/credentials/v2
     additionalItems:
       type: string
       enum:
@@ -33,70 +33,70 @@ properties:
         - https://ref.gs1.org/gs1/vc/trade-item-context/
         - https://ref.gs1.org/gs1/vc/declaration-context/
 `)
-  );
+  )
 
   it('minimal context', async () => {
     const instance1 = {
-      '@context': ['https://www.w3.org/2018/credentials/v2'],
-    };
-    const validation1 = validate(instance1);
-    expect(validation1).toBe(true);
-  });
+      '@context': ['https://www.w3.org/ns/credentials/v2'],
+    }
+    const validation1 = validate(instance1)
+    expect(validation1).toBe(true)
+  })
 
   describe('extended contexts', () => {
     describe('valid', () => {
       it('https://w3id.org/traceability/v1', async () => {
         const instance1 = {
           '@context': [
-            'https://www.w3.org/2018/credentials/v2',
+            'https://www.w3.org/ns/credentials/v2',
             'https://w3id.org/traceability/v1',
           ],
-        };
-        const validation1 = validate(instance1);
-        expect(validation1).toBe(true);
-      });
+        }
+        const validation1 = validate(instance1)
+        expect(validation1).toBe(true)
+      })
       it('https://w3id.org/vc/status-list/2021/v1', async () => {
         const instance1 = {
           '@context': [
-            'https://www.w3.org/2018/credentials/v2',
+            'https://www.w3.org/ns/credentials/v2',
             'https://w3id.org/vc/status-list/2021/v1',
           ],
-        };
-        const validation1 = validate(instance1);
-        expect(validation1).toBe(true);
-      });
+        }
+        const validation1 = validate(instance1)
+        expect(validation1).toBe(true)
+      })
       it('https://ref.gs1.org/gs1/vc/license-context/', async () => {
         const instance1 = {
           '@context': [
-            'https://www.w3.org/2018/credentials/v2',
+            'https://www.w3.org/ns/credentials/v2',
             'https://ref.gs1.org/gs1/vc/license-context/',
           ],
-        };
-        const validation1 = validate(instance1);
-        expect(validation1).toBe(true);
-      });
-    });
+        }
+        const validation1 = validate(instance1)
+        expect(validation1).toBe(true)
+      })
+    })
     describe('invalid', () => {
       it('unknown url', async () => {
         const instance1 = {
           '@context': [
-            'https://www.w3.org/2018/credentials/v2',
+            'https://www.w3.org/ns/credentials/v2',
             'https://extension.example/v42', // not in allow list
           ],
-        };
-        const validation1 = validate(instance1);
-        expect(validation1).toBe(false);
-      });
+        }
+        const validation1 = validate(instance1)
+        expect(validation1).toBe(false)
+      })
       it('unknown object', async () => {
         const instance2 = {
           '@context': [
-            'https://www.w3.org/2018/credentials/v2',
+            'https://www.w3.org/ns/credentials/v2',
             { '@vocab': 'https://extension.example/#' }, // not in allow list
           ],
-        };
-        const validation2 = validate(instance2);
-        expect(validation2).toBe(false);
-      });
-    });
-  });
-});
+        }
+        const validation2 = validate(instance2)
+        expect(validation2).toBe(false)
+      })
+    })
+  })
+})
